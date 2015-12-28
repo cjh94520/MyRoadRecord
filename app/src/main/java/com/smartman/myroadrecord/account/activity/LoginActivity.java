9e747c71435d98c692a978639b04721f625bbf60
jiahui <892546937@qq.com>
2015-12-28 20:36:13 +0800

    目录调整

---
+++ app/src/main/java/com/smartman/myroadrecord/account/activity/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.smartman.myroadrecord.account.activity;
+
+import com.smartman.myroadrecord.activity.BaseActivity;
+
+/**
+ * Created by jiahui.chen on 2015/12/28.
+ */
+public class LoginActivity extends BaseActivity {
+
+}
