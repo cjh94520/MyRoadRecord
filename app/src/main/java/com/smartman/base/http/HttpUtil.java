d4f7c3d87c4bfe9b8f3ede6c8f5633fe1262cf0f
jiahui <892546937@qq.com>
2015-12-29 19:23:25 +0800

    http get post 同步异步方法添加

--- app/src/main/java/com/smartman/base/http/HttpUtil.java
+++ app/src/main/java/com/smartman/base/http/HttpUtil.java
@@ -1,15 +1,77 @@
 package com.smartman.base.http;
 
-import com.smartman.base.exception.TaskException;
-
+import org.xutils.common.Callback;
 import org.xutils.http.RequestParams;
+import org.xutils.x;
 
 /**
  * Created by jiahui.chen on 2015/12/29.
  */
 public class HttpUtil {
-    public static <T> T doGet(String actionSetting, RequestParams params, Class<T> responseCls) throws TaskException {
+    public static <T> T doGetSync(RequestParams params, Class<T> responseCls) {
+        try {
+            return x.http().getSync(params, responseCls);
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
         return null;
     }
+
+    public static void doGet(RequestParams params) {
+        x.http().get(params, new Callback.CommonCallback<Object>() {
+            @Override
+            public void onSuccess(Object result) {
+
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
+    }
+
+    public static <T> T doPostSync(RequestParams params, Class<T> responseCls) {
+        try {
+            return x.http().postSync(params, responseCls);
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void doPost(RequestParams params) {
+        x.http().post(params, new Callback.CommonCallback<Object>() {
+            @Override
+            public void onSuccess(Object result) {
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
+    }
+
 
 }
--- app/src/main/java/com/smartman/myroadrecord/business/account/accountMgmt.java
+++ app/src/main/java/com/smartman/myroadrecord/business/account/accountMgmt.java
@@ -1,54 +1,25 @@
 package com.smartman.myroadrecord.business.account;
 
 import com.smartman.base.exception.TaskException;
+import com.smartman.base.http.HttpUtil;
+import com.smartman.base.utils.SettingUtil;
+import com.smartman.myroadrecord.business.account.bean.AccountBean;
 
-import org.xutils.common.Callback;
 import org.xutils.http.RequestParams;
-import org.xutils.x;
 
 /**
  * Created by jiahui.chen on 2015/12/29.
  */
 public class accountMgmt {
 
-    private String getUserInfoURI()
-    {
-        return "http://www.baidu.com";
-    }
     /**
      * @return AccountBean
      * @Description: 获取用户信息
      */
-    public void getUserInfo() throws TaskException {
-        String uri = getUserInfoURI();
+    public AccountBean getUserInfo() throws TaskException {
+        String uri = SettingUtil.getStringSetting("url_china");
         RequestParams params = new RequestParams(uri);
-        params.addQueryStringParameter("userID","18688553035");
-        x.http().get(params, new Callback.CacheCallback<String>() {
-            @Override
-            public void onSuccess(String result) {
-
-            }
-
-            @Override
-            public void onError(Throwable ex, boolean isOnCallback) {
-
-            }
-
-            @Override
-            public void onCancelled(CancelledException cex) {
-
-            }
-
-            @Override
-            public void onFinished() {
-
-            }
-
-            @Override
-            public boolean onCache(String result) {
-                return false;
-            }
-        });
-
+        params.addQueryStringParameter("userID", "18688553035");
+        return HttpUtil.doGetSync(params, AccountBean.class);
     }
 }
