8d228e0deb423c37c2f60d2f96ffc604c29e29e8
jiahui.chen <892546937@qq.com>
2016-02-03 12:26:17 +0800

    加入微信分享包，转换类

--- app/src/main/java/com/smartman/base/utils/ConvertUtil.java
+++ app/src/main/java/com/smartman/base/utils/ConvertUtil.java
@@ -3,7 +3,17 @@ package com.smartman.base.utils;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+import org.xutils.common.util.LogUtil;
+
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by jiahui.chen on 2016/2/2.
@@ -11,11 +21,19 @@ import java.io.ByteArrayOutputStream;
  */
 public class ConvertUtil {
 
-    public static byte[] Bitmap2Bytes(Bitmap bm) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        //Bitmap转换成PNG的格式
-        bm.compress(Bitmap.CompressFormat.PNG, 100, baos);
-        return baos.toByteArray();
+    public static byte[] Bitmap2Bytes(Bitmap bmp, Boolean needRecycle) {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, output);
+        if (needRecycle) {
+            bmp.recycle();
+        }
+        byte[] result = output.toByteArray();
+        try {
+            output.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     public static Bitmap Bytes2Bimap(byte[] b) {
@@ -24,6 +42,88 @@ public class ConvertUtil {
         } else {
             return null;
         }
+    }
+
+    public static byte[] getHtmlByteArray(final String url) {
+        URL htmlUrl = null;
+        InputStream inStream = null;
+        try {
+            htmlUrl = new URL(url);
+            URLConnection connection = htmlUrl.openConnection();
+            HttpURLConnection httpConnection = (HttpURLConnection) connection;
+            int responseCode = httpConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                inStream = httpConnection.getInputStream();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        byte[] data = inputStreamToByte(inStream);
+
+        return data;
+    }
+
+    public static byte[] inputStreamToByte(InputStream is) {
+        try {
+            ByteArrayOutputStream bytestream = new ByteArrayOutputStream();
+            int ch;
+            while ((ch = is.read()) != -1) {
+                bytestream.write(ch);
+            }
+            byte imgdata[] = bytestream.toByteArray();
+            bytestream.close();
+            return imgdata;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static byte[] readFromFile(String fileName, int offset, int len) {
+        if (fileName == null) {
+            return null;
+        }
+        File file = new File(fileName);
+        if (!file.exists()) {
+            LogUtil.i("readFromFile: file not found");
+            return null;
+        }
+
+        if (len == -1) {
+            len = (int) file.length();
+        }
+
+        LogUtil.d("readFromFile : offset = " + offset + " len = " + len + " offset + len = " + (offset + len));
+
+        if (offset < 0) {
+            LogUtil.e("readFromFile invalid offset:" + offset);
+            return null;
+        }
+        if (len <= 0) {
+            LogUtil.e("readFromFile invalid len:" + len);
+            return null;
+        }
+        if (offset + len > (int) file.length()) {
+            LogUtil.e("readFromFile invalid file len:" + file.length());
+            return null;
+        }
+
+        byte[] b = null;
+        try {
+            RandomAccessFile in = new RandomAccessFile(fileName, "r");
+            b = new byte[len]; // 创建合适文件大小的数组
+            in.seek(offset);
+            in.readFully(b);
+            in.close();
+
+        } catch (Exception e) {
+            LogUtil.e("readFromFile : errMsg = " + e.getMessage());
+            e.printStackTrace();
+        }
+        return b;
     }
 
 
--- app/src/main/java/com/smartman/myroadrecord/module/account/activity/AccessActivity.java
+++ app/src/main/java/com/smartman/myroadrecord/module/account/activity/AccessActivity.java
@@ -1,17 +1,134 @@
 package com.smartman.myroadrecord.module.account.activity;
 
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.smartman.base.activity.BaseActivity;
+import com.smartman.base.task.TaskException;
+import com.smartman.base.task.WorkTask;
+import com.smartman.base.ui.CircleImageView;
+import com.smartman.base.utils.DialogUtil;
+import com.smartman.base.utils.PrefsUtil;
+import com.smartman.base.utils.ResourceUtil;
+import com.smartman.base.utils.ToastUtil;
 import com.smartman.myroadrecord.R;
+import com.smartman.myroadrecord.base.activity.MainActivity;
+import com.smartman.myroadrecord.business.account.accountMgmt;
+import com.smartman.myroadrecord.module.account.event.UserEvent;
+import com.smartman.myroadrecord.module.account.param.UserConst;
+import com.smartman.myroadrecord.module.account.util.UserUtil;
 
+import org.xutils.common.util.LogUtil;
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
+import org.xutils.view.annotation.ViewInject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import de.greenrobot.event.EventBus;
 
 @ContentView(R.layout.activity_access)
 public class AccessActivity extends BaseActivity {
+    @ViewInject(R.id.access_btn)
+    private Button accessBtn;
+    @ViewInject(R.id.access_img)
+    private CircleImageView imgView;
+    @ViewInject(R.id.access_name)
+    private EditText nameText;
+
+    private static final int CHANGE_IMG = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Event(value = R.id.access_img, type = DialogInterface.OnClickListener.class)
+    private void imgViewClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        intent.putExtra("crop", true);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, CHANGE_IMG);
+    }
+
+    @Event(value = R.id.access_btn, type = DialogInterface.OnClickListener.class)
+    private void accessClick(View view) {
+        Intent intent = new Intent(AccessActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        PrefsUtil.savePrefString(UserConst.USER_NAME, nameText.getText().toString());
+        // 发布事件，在后台线程发的事件
+        EventBus.getDefault().post(new UserEvent());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == CHANGE_IMG) {
+                Uri uri = data.getData();
+                LogUtil.d(uri.getPath());
+                ContentResolver cr = this.getContentResolver();
+                try {
+                    Bitmap bmp = BitmapFactory.decodeStream(cr.openInputStream(uri));
+                    //压缩大约有30倍
+                    Bitmap target = ThumbnailUtils.extractThumbnail(bmp, imgView.getWidth(), imgView.getHeight(), ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
+                    bmp.recycle();
+                    File file = UserUtil.saveBitmap2file(target);
+                    new uploadImgTask().execute(file.getPath());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+    }
+
+    class uploadImgTask extends WorkTask<String, Void, Boolean> {
+        String mFilePath = null;
+
+        @Override
+        protected void onPrepare() {
+            super.onPrepare();
+            DialogUtil.showProgressDialog(AccessActivity.this, ResourceUtil.getString(R.string.uploadingImg), false);
+        }
+
+        @Override
+        public Boolean workInBackground(String... params) throws TaskException {
+            mFilePath = params[0];
+            return new accountMgmt().uploadImg(params[0]);
+        }
+
+        @Override
+        protected void onSuccess(Boolean s) {
+            super.onSuccess(s);
+            Bitmap bmp = BitmapFactory.decodeFile(mFilePath);
+            imgView.setImageBitmap(bmp);
+        }
+
+        @Override
+        protected void onFailure(TaskException exception) {
+            super.onFailure(exception);
+            ToastUtil.showMessage(R.string.upload_fail);
+            LogUtil.d(exception.getMessage());
+        }
+
+        @Override
+        protected void onFinished() {
+            super.onFinished();
+            DialogUtil.dismissNormalProgressDialog();
+        }
     }
 }
